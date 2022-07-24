3b35aa3a5ec302f57bc9515699315e05a21ddfdb
Anjarman200 <anjarbuncit@gmail.com>
2022-07-24 19:40:38 +0700

    main

---
+++ Kitzuna.js
@@ -0,0 +1,21 @@
+//━━━━━━━━━━━━━━━[ PERINGATAN ]━━━━━━━━━━━━━━━━━//
+Hai Gw Selaku Pembuat Nih Sc Berterima Kasih
+ Kepada:
+
+あ Anjarman20 [Author]
+あ Man [Newbie]
+あ Kula [Mastah]
+あ Mirta [Sensei]
+あ Penyedia Module 
+あ Penyedia Rest Api
+
+Note: Jangan Pernah Jual Nih Scrip Bot
+Ketahuan Jual Nih Sc Gw Gak Bakal
+Upload Scrip Bot WhatsApp 
+
+Boleh Ambil Fitur Tp Minimal Add Nama Gw Lah
+//━━━━━━━━━━━━━━━[ AKHIR ]━━━━━━━━━━━━━━━━━//
+Oke Makasih Banyak Yang Udah Support
+Channel YouTube Gw Dan Jangan Lupa 
+Like, Subscribe And Share 
+//------------------------------------------GABUTZ-BANG-----------------------------------------//
